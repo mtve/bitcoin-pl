8dd38ecdb7398f45e1a7e1242eac363b7c064983
mtve <mtve1927@gmail.com>
2011-04-01 18:08:17 +0400

    chain branches and few fixes

--- util.pm
+++ util.pm
@@ -1,9 +1,35 @@
-package pem;
+package util;
 
 use warnings;
 use strict;
 
 sub D() { 0 }
+
+# bin2hex
+
+sub util::bin2hex::TIEHASH { bless {}, $_[0] }
+sub util::bin2hex::FETCH { unpack 'H*', reverse $_[1] }
+
+tie our %b2h, 'util::bin2hex';
+
+# hex2bin
+
+sub util::hex2bin::TIEHASH { bless {}, $_[0] }
+sub util::hex2bin::FETCH { reverse pack 'H*', $_[1] }
+
+tie our %h2b, 'util::hex2bin';
+
+# html_esc
+
+sub util::html_esc::TIEHASH { bless {}, $_[0] }
+sub util::html_esc::FETCH {
+	my (undef, $str) = @_;
+
+	s/&/&amp;/g, s/</&lt;/g, s/>/&gt;/g, s/"/&quot;/g for $str;
+	return $str;
+}
+
+tie our %hesc, 'util::html_esc';
 
 #
 # asn.1
